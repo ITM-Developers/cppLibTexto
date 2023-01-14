61bef444babbed0193299960f27e1a968ddcc0a3
ITM Developers <itm.developers.mx@gmail.com>
2023-01-13 20:15:36 -0600

    Initial commit

---
+++ libtexto.cpp
@@ -0,0 +1,5 @@
+#include "libtexto.hpp"
+
+LibTexto::LibTexto()
+{
+}
---
+++ libtexto.hpp
@@ -0,0 +1,12 @@
+#ifndef LIBTEXTO_HPP
+#define LIBTEXTO_HPP
+
+#include "LibTexto_global.h"
+
+class LIBTEXTO_EXPORT LibTexto
+{
+public:
+    LibTexto();
+};
+
+#endif // LIBTEXTO_HPP
